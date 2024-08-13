65f98f59872bbccb74dd19b3f84b460ac7755bd1
Hydrateurself <bensn@posteo.de>
2024-08-13 16:59:34 +0200

    counter turns 180deg on weiter

--- Rummy-Counter/src/App.tsx
+++ Rummy-Counter/src/App.tsx
@@ -1,51 +1,73 @@
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import { Button } from "./components/Button";
 import "./index.css";
 function App() {
-  const [counter, setCounter] = useState<number>(3);
-  const startCountJohn = () => {
-    const interval = setInterval(
+  const [counter, setCounter] = useState<number>(63);
+  const [isRotated, setIsRotated] = useState(false);
+  const intervalRef = useRef<number | null>(null);
+  const startCount = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+
+    intervalRef.current = setInterval(
       () =>
         setCounter((counter) => {
           if (counter > 0) {
             return counter - 1;
           } else {
-            clearInterval(interval);
-            return 3;
+            clearInterval(intervalRef.current!);
+
+            return 63;
           }
         }),
       1000
     );
   };
 
-  const startCountBene = () => {
-    const interval = setInterval(
-      () =>
-        setCounter((counter) => {
-          if (counter > 0) {
-            return counter - 1;
-          } else {
-            clearInterval(interval);
-            return 3;
-          }
-        }),
-      1000
-    );
+  const handleClick = () => {
+    setCounter(63);
+    setIsRotated(!isRotated);
+    startCount();
   };
 
+  const firstCLick = () => {
+    startCount();
+  };
+
+  const stop = () => {
+    setCounter(63);
+    clearInterval(intervalRef.current!);
+  };
   return (
-    <div className="flex w-full h-screen flex-col justify-evenly items-center pl-4 pr-4">
-      <h2>Rummy Cub</h2>
-      <Button color={"green"} style={""} onClick={startCountJohn}>
-        Button 1
+    <div className="flex w-full h-screen flex-col justify-evenly items-center pl-4 pr-4 bg-blue-300">
+      <h2 className="font-abril text-5xl rotate-180">Rummyclub</h2>
+      <Button color={"yellow"} style={""} onClick={handleClick} rotate="180deg">
+        Weiter
       </Button>
-      <div className="w-full h-1/2 bg-black text-white flex justify-center items-center">
-        {counter}
+      <div className="flex justify-center items-center">
+        <div className="w-auto h-auto text-black flex justify-center items-center text-8xl">
+          <div
+            className={`${
+              isRotated ? "rotate-180" : " "
+            } font-abril font-bold pl-6 pr-6`}
+          >
+            {counter}
+          </div>
+        </div>
       </div>
-      <Button color={"red"} style={""} onClick={startCountBene}>
-        Button 2
+      <Button color={"yellow"} style={""} onClick={handleClick} rotate="">
+        Weiter
       </Button>
-      <h2>Rummy Cub</h2>
+      <div className="flex gap-8">
+        <Button color="green" style="" onClick={firstCLick} rotate="">
+          Go
+        </Button>
+        <Button color={"red"} style="" onClick={stop} rotate="">
+          No
+        </Button>
+      </div>
+      <h2 className="font-abril text-5xl">Rummyclub</h2>
     </div>
   );
 }
--- Rummy-Counter/src/components/Button.tsx
+++ Rummy-Counter/src/components/Button.tsx
@@ -3,14 +3,20 @@ type Props = {
   onClick: () => void;
   color: string;
   style: string;
+  rotate: string;
 };
 
-export function Button({ children, color, onClick }: Props) {
+export function Button({ children, color, onClick, rotate }: Props) {
   const buttonStyle = {
     backgroundColor: color,
+    rotate: rotate,
   };
   return (
-    <button style={buttonStyle} onClick={onClick}>
+    <button
+      style={buttonStyle}
+      onClick={onClick}
+      className="pl-6 pr-6 pt-2 pb-2 rounded-full border-2 border-red-300 font-archivo font-bold"
+    >
       {children}
     </button>
   );
